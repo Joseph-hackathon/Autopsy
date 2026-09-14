6a94b5d3681830a6e07918ca6c6fd8a4eb603929
Joseph-hackathon <sjcho.hackathon@gmail.com>
2026-09-14 15:42:32 +0900

    chore: remove temporary debug and update scripts

--- test_id2.py
+++
@@ -1,8 +0,0 @@
-﻿import sys
-import requests
-sys.path.append('backend')
-from cmc_api import CMCClient, HEADERS, BASE_URL
-
-url = f"{BASE_URL}/v1/cryptocurrency/info"
-res = requests.get(url, headers=HEADERS, params={"id": 32544})
-print(res.json())
--- test_slug.py
+++
@@ -1,5 +0,0 @@
-﻿import sys
-sys.path.append('backend')
-from cmc_api import CMCClient
-
-print(CMCClient.get_info("ftx-token"))
